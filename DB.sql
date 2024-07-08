19fd086fe1ffb11a4d8013c7e20d4fbd04b2c868
es1210 <dmstn27034@gmail.com>
2024-07-08 15:24:26 +0900

    JDBC INSERT test

--- DB.sql
+++ DB.sql
@@ -3,3 +3,24 @@ CREATE DATABASE `AM_JDBC_2024_07`;
 USE `AM_JDBC_2024_07`;
 
 SHOW TABLES;
+
+CREATE TABLE article(
+                        id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        regDate DATETIME NOT NULL,
+                        updateDate DATETIME NOT NULL,
+                        title CHAR(100) NOT NULL,
+                        `body` TEXT NOT NULL
+);
+
+#랜덤 숫자 생성(1~100)
+SELECT SUBSTRING(RAND() * 1000 FROM 1 FOR 2)
+
+SELECT *
+FROM article;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
