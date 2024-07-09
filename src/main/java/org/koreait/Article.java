4cbaac1e5f1454b85f80c6d7ac1c2558d599add0
es1210 <dmstn27034@gmail.com>
2024-07-09 14:23:33 +0900

    JDBC Select 테스트

--- src/main/java/org/koreait/Article.java
+++ src/main/java/org/koreait/Article.java
@@ -2,45 +2,64 @@ package org.koreait;
 
 public class Article {
     private int id;
+    private String regDate;
+    private String updateDate;
     private String title;
-    private String content;
+    private String body;
+
+    public Article(int id, String regDate, String updateDate, String title, String body) {
+        this.id = id;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
 
     @Override
     public String toString() {
         return "Article{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
+                ", body='" + body + '\'' +
                 '}';
     }
-
-    public Article(int id, String title, String content) {
+    public Article(int id, String title, String body) {
         this.id = id;
         this.title = title;
-        this.content = content;
+        this.body = body;
     }
-
-    public int getId() {
-        return id;
+    public String getBody() {
+        return body;
     }
-
-    public void setId(int id) {
-        this.id = id;
+    public void setBody(String body) {
+        this.body = body;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public String getContent() {
-        return content;
+    public int getId() {
+        return id;
     }
-
-    public void setContent(String content) {
-        this.content = content;
+    public void setId(int id) {
+        this.id = id;
     }
 }
