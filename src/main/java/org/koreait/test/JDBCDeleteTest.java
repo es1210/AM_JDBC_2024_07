abb90f66ea707cf5b270deb4be8164ab5b0c2f59
es1210 <dmstn27034@gmail.com>
2024-07-09 16:52:26 +0900

    SQLErrorException, DBUtil, SecSql 추가

--- src/main/java/org/koreait/test/JDBCDeleteTest.java
+++ src/main/java/org/koreait/test/JDBCDeleteTest.java
@@ -1,12 +1,14 @@
-package org.koreait;
+package org.koreait.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBCConnTest {
+public class JDBCDeleteTest {
     public static void main(String[] args) {
         Connection conn = null;
+        PreparedStatement pstmt = null;
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
@@ -14,6 +16,14 @@ public class JDBCConnTest {
             //여기 AM_JDBC_2024_07를 잘 써줘야함 ! (연결된 DB에 존재해야함(DataBase로))
             conn = DriverManager.getConnection(url, "root", "");
             System.out.println("연결 성공!");
+
+            String sql = "DELETE FROM article WHERE id=1";
+
+            pstmt = conn.prepareStatement(sql);
+
+            int affectedRows = pstmt.executeUpdate();
+
+            System.out.println("affected rows: " + affectedRows);
 
         } catch (ClassNotFoundException e) {
             System.out.println("드라이버 로딩 실패" + e);
@@ -23,6 +33,13 @@ public class JDBCConnTest {
             try {
                 if (conn != null && !conn.isClosed()) {
                     conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null && !pstmt.isClosed()) {
+                    pstmt.close();
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
