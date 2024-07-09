8be38aaf86666ff03a12d09ea693a5c31a70a0be
es1210 <dmstn27034@gmail.com>
2024-07-09 15:27:22 +0900

    게시글 수정 기능 구현, 글의 유무 체크 x

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,5 +1,4 @@
 package org.koreait;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,9 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("==프로그램 시작==");
         Scanner sc = new Scanner(System.in);
-
         int lastArticleId = 0;
-
         while (true) {
             System.out.print("명령어 > ");
             String cmd = sc.nextLine().trim();
@@ -24,11 +21,8 @@ public class Main {
                 String title = sc.nextLine();
                 System.out.print("내용 : ");
                 String body = sc.nextLine();
-
-
                 lastArticleId++;
                 System.out.println(id + "번 글이 작성되었습니다");
-
                 //////////////////////////////
                 Connection conn = null;
                 PreparedStatement pstmt = null;
@@ -52,95 +46,151 @@ public class Main {
                     System.out.println("에러 : " + e);
                 } finally {
                     try {
-                        if (conn != null && !conn.isClosed()) {
-                            conn.close();
+                            if (pstmt != null && !pstmt.isClosed()) {
+                                pstmt.close();
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
                         }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (pstmt != null && !pstmt.isClosed()) {
-                            pstmt.close();
+                        try {
+                                if (conn != null && !conn.isClosed()) {
+                                    conn.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
+                    } else if (cmd.equals("article list")) {
+                        System.out.println("==목록==");
+                        Connection conn = null;
+                        PreparedStatement pstmt = null;
+                        ResultSet rs = null;
+                        List<Article> articles = new ArrayList<>();
+                        try {
+                            Class.forName("org.mariadb.jdbc.Driver");
+                            String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                            conn = DriverManager.getConnection(url, "root", "");
+                            System.out.println("연결 성공!");
+                            String sql = "SELECT * ";
+                            sql += "FROM article ";
+                            sql += "ORDER BY id DESC";
+                            System.out.println(sql);
+                            pstmt = conn.prepareStatement(sql);
+                            rs = pstmt.executeQuery(sql);
+                            while (rs.next()) {
+                                int id = rs.getInt("id");
+                                String regDate = rs.getString("regDate");
+                                String updateDate = rs.getString("updateDate");
+                                String title = rs.getString("title");
+                                String body = rs.getString("body");
+                                Article article = new Article(id, regDate, updateDate, title, body);
+                                articles.add(article);
+                            }
+                        } catch (ClassNotFoundException e) {
+                            System.out.println("드라이버 로딩 실패" + e);
+                        } catch (SQLException e) {
+                            System.out.println("에러 : " + e);
+                        } finally {
+                            try {
+                                if (rs != null && !rs.isClosed()) {
+                                    rs.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                if (pstmt != null && !pstmt.isClosed()) {
+                                    pstmt.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                if (conn != null && !conn.isClosed()) {
+                                    conn.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (articles.size() == 0) {
+                            System.out.println("게시글이 없습니다");
+                            continue;
+                        }
+                        System.out.println("   번호    /    제목     ");
+                        for (Article article : articles) {
+                            System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
+                        }
+
+                    } else if (cmd.startsWith("article modify")) {
+
+                        int id = 0;
+
+                        try {
+                            id = Integer.parseInt(cmd.split(" ")[2]);
+                        } catch (Exception e) {
+                            System.out.println("정수 입력");
+                            continue;
+                        }
+
+                        System.out.println("==글 수정==");
+                        System.out.print("새 제목 : ");
+                        String title = sc.nextLine();
+                        System.out.print("새 내용 : ");
+                        String body = sc.nextLine();
+
+                        //////////////////////////////
+
+                        Connection conn = null;
+                        PreparedStatement pstmt = null;
+
+                        try {
+                            Class.forName("org.mariadb.jdbc.Driver");
+                            String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                            conn = DriverManager.getConnection(url, "root", "");
+                            System.out.println("연결 성공!");
+
+                            String sql = "UPDATE article ";
+                            sql += "SET updateDate = NOW()";
+                            if (title.length() > 0) {
+                                sql += " , title = '" + title + "'";
+                            }
+                            if (body.length() > 0) {
+                                sql += " , `body` = '" + body + "'";
+                            }
+                            sql += " WHERE id = " + id + ";";
+
+                            System.out.println(sql);
+
+                            pstmt = conn.prepareStatement(sql);
+
+                            pstmt.executeUpdate();
+
+
+                        } catch (ClassNotFoundException e) {
+                            System.out.println("드라이버 로딩 실패" + e);
+                        } catch (SQLException e) {
+                            System.out.println("에러 : " + e);
+                        } finally {
+                            try {
+                                if (pstmt != null && !pstmt.isClosed()) {
+                                    pstmt.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                if (conn != null && !conn.isClosed()) {
+                                    conn.close();
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        System.out.println(id + "번 글이 수정되었습니다.");
                     }
                 }
 
-            } else if (cmd.equals("article list")) {
-                System.out.println("==목록==");
-
-                Connection conn = null;
-                PreparedStatement pstmt = null;
-                ResultSet rs = null;
-
-                List<Article> articles = new ArrayList<>();
-
-                try {
-                    Class.forName("org.mariadb.jdbc.Driver");
-                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
-                    conn = DriverManager.getConnection(url, "root", "");
-                    System.out.println("연결 성공!");
-
-                    String sql = "SELECT * ";
-                    sql += "FROM article ";
-                    sql += "ORDER BY id DESC";
-
-                    System.out.println(sql);
-                    pstmt = conn.prepareStatement(sql);
-                    rs = pstmt.executeQuery(sql);
-
-                    while (rs.next()) {
-                        int id = rs.getInt("id");
-                        String regDate = rs.getString("regDate");
-                        String updateDate = rs.getString("updateDate");
-                        String title = rs.getString("title");
-                        String body = rs.getString("body");
-                        Article article = new Article(id, regDate, updateDate, title, body);
-                        articles.add(article);
-                    }
-
-                } catch (ClassNotFoundException e) {
-                    System.out.println("드라이버 로딩 실패" + e);
-                } catch (SQLException e) {
-                    System.out.println("에러 : " + e);
-                } finally {
-                    try {
-                        if (rs != null && !rs.isClosed()) {
-                            rs.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (pstmt != null && !pstmt.isClosed()) {
-                            pstmt.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        if (conn != null && !conn.isClosed()) {
-                            conn.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (articles.size() == 0) {
-                    System.out.println("게시글이 없습니다");
-                    continue;
-                }
-                System.out.println("   번호    /    제목     ");
-                for (Article article : articles) {
-                    System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
-                }
-
+                System.out.println("==프로그램 종료==");
             }
-
-
         }
-
-        System.out.println("==프로그램 종료==");
-    }
-}
