f503e97a398ff6db26da799089400ac4ed67b725
es1210 <dmstn27034@gmail.com>
2024-07-15 13:48:04 +0900

    회원가입 기능 구현

--- DB.sql
+++ DB.sql
@@ -1,18 +1,24 @@
-DROP DATABASE IF EXISTS `AM_JDBC_2024_07`;
-CREATE DATABASE `AM_JDBC_2024_07`;
-USE `AM_JDBC_2024_07`;
+DROP
+DATABASE IF EXISTS `AM_JDBC_2024_07`;
+CREATE
+DATABASE `AM_JDBC_2024_07`;
+USE
+`AM_JDBC_2024_07`;
 
-SHOW TABLES;
+SHOW
+TABLES;
 
-CREATE TABLE article(
-                        id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        regDate DATETIME NOT NULL,
-                        updateDate DATETIME NOT NULL,
-                        title CHAR(100) NOT NULL,
-                        `body` TEXT NOT NULL
+CREATE TABLE article
+(
+    id         INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate    DATETIME  NOT NULL,
+    updateDate DATETIME  NOT NULL,
+    title      CHAR(100) NOT NULL,
+    `body`     TEXT      NOT NULL
 );
 
-#랜덤 숫자 생성(1~100)
+#랜덤
+숫자 생성(1~100)
 SELECT SUBSTRING(RAND() * 1000 FROM 1 FOR 2)
 
 SELECT *
@@ -24,3 +30,40 @@ updateDate = NOW(),
 title = '제목1',
 `body` = '내용1';
 
+CREATE TABLE `member`
+(
+    id         INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate    DATETIME  NOT NULL,
+    updateDate DATETIME  NOT NULL,
+    loginId    CHAR(30)  NOT NULL,
+    loginPw    CHAR(200) NOT NULL,
+    `name`     CHAR(100) NOT NULL
+);
+
+SELECT *
+FROM article;
+
+SELECT *
+FROM `member`;
+
+###############################################################################
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
+`body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '김철수';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '홍길동';
